2fa60a40b05d2f568d1827050c13901dcf0bc2ff
Eric M. Seitz <eric.seitz@gmail.com>
2021-03-05 13:58:56 -0800

    finish setting up contentView and AddTodoview

--- DoneList/DoneList/View/AddTodoView.swift
+++ DoneList/DoneList/View/AddTodoView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct AddTodoView: View {
     //: MARK - PROPERTIES
     
+    @Environment(\.presentationMode) var presentationMode
+    
     @State private var name: String = ""
     @State private var priority: String = "Normal"
     
@@ -36,6 +38,12 @@ struct AddTodoView: View {
                 Spacer()
             } // END VSTACK
             .navigationBarTitle("New Todo", displayMode: .inline)
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }) {
+                                        Image(systemName: "xmark")
+                                    })
         } // END NAVIGATION
     }
 }
--- DoneList/DoneList/View/ContentView.swift
+++ DoneList/DoneList/View/ContentView.swift
@@ -9,70 +9,30 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    // MARK: - PROPERTIES
+    @State private var showingAddTodoView: Bool = false
+    
+    // MARK: - BODY
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationView {
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Text("Hello World")
+            } //: END LIST
+            .navigationBarTitle("Todo", displayMode: .inline)
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.showingAddTodoView.toggle()
+                                    }) {
+                                        Image(systemName: "plus")
+                                    })//: END ADD BUTTON
+            .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView()
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        } //: END NAVIGATION
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
